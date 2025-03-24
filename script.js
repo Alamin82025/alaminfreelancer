1f44a722cf1586005467fa181d27c8df81920a1e
Alamin82025 <mdalaminfrelancer@gmail.com>
2025-03-24 15:07:57 +0600

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JavaScript for form validation (if you want)
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Form submitted successfully!");
+});
